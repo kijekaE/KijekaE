3d353239721ff834721e3bef45bf9afdaf4c33e1
TheJagStudio <thejagstudio@gmail.com>
2023-03-13 21:41:50 +0530

    New

--- kijeka/settings.py
+++ kijeka/settings.py
@@ -34,7 +34,6 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
-Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
