01a301912bb9cc4c440c88240088d1a1d4b41930
Vrushank <Vrushank@pruthatek.com>
2026-06-24 13:06:38 +0530

    fix: added new build

---
+++ static/js/655.1feb59df.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkkijeka=self.webpackChunkkijeka||[]).push([[655],{4655:(e,k,s)=>{s.r(k),s.d(k,{default:()=>a});const a={}}}]);
+//# sourceMappingURL=655.1feb59df.chunk.js.map
